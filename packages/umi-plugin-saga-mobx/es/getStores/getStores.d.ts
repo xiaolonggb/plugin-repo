87f365112b8bcca212224e69eb35d40568390d46
xiaolonggb <123525906@qq.com>
2022-04-18 16:08:15 +0800

    feat: 更新.gitignore

--- packages/umi-plugin-saga-mobx/es/getStores/getStores.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function getStores(opts: {
-    base: string;
-    cwd: string;
-    pattern?: string;
-    skipModelValidate?: boolean;
-    extraModels?: string[];
-}): string[];
--- packages/umi-plugin-saga-mobx/lib/getUserLibDir.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function getUserLibDir({ library, pkg, cwd, }: {
-    library: string;
-    pkg: any;
-    cwd: string;
-}): string;
